7719118e59bd9d9ccbf3d0b2245a00529985917b
alizaynoune <zaynoune.ali@gmail.com>
2022-03-04 13:13:21 +0100

    add storEnhancers
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers";
 import socketMiddleware from "../middleware/middleware";
 
 const middleware = [socketMiddleware, thunk];
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
   rootReducer,
-  applyMiddleware(...middleware)
+  storeEnhancers(applyMiddleware(...middleware))
 );
